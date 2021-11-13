0443d511d990fb42ab99f23ea7ac65b5a71c4efe
AL-Shahriar <anik7744@gmail.com>
2021-11-13 13:35:10 +0600

    Fixed admin add issue

--- index.js
+++ index.js
@@ -67,24 +67,24 @@ async function run() {
             const result = usersCollection.updateOne(filter,updateDoc,options);
                 res.json(result);
         })
-        // app.get('/users/:email', async(req,res) =>{
-        //     const email = req.params.email;
-        //     const query = {email: email};
-        //     const user = await usersCollection.findOne(query);
-        //     let isAdmin = false;
-        //     if(user?.role === 'admin'){
-        //         isAdmin= true;
-        //     }
-        //     res.json({admin: isAdmin});
-        // })
-        // app.put('/users/admin',async(req,res)=>{
-        //     const user = req.body; 
-        //     const filter = {email: user.email};
-        //     const updateDoc = {$set: {role: 'admin'}};
-        //     const result = await usersCollection.updateOne(filter,updateDoc);
-        //     res.json(result);
+        app.get('/users/:email', async(req,res) =>{
+            const email = req.params.email;
+            const query = {email: email};
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if(user?.role === 'admin'){
+                isAdmin= true;
+            }
+            res.json({admin: isAdmin});
+        })
+        app.put('/users/admin',async(req,res)=>{
+            const user = req.body; 
+            const filter = {email: user.email};
+            const updateDoc = {$set: {role: 'admin'}};
+            const result = await usersCollection.updateOne(filter,updateDoc);
+            res.json(result);
 
-        // })
+        })
     }
     finally {
 
