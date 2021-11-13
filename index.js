ce8d126fb7a609bc6d2bcf89f2a861daa5544352
AL-Shahriar <anik7744@gmail.com>
2021-11-13 13:29:30 +0600

    added registered

--- index.js
+++ index.js
@@ -58,24 +58,33 @@ async function run() {
             const result = await usersCollection.insertOne(user);
             res.json(result);
         })
-        app.get('/users/:email', async(req,res) =>{
-            const email = req.params.email;
-            const query = {email:email};
-            const user = await usersCollection.findOne(query);
-            let isAdmin = false;
-            if(user?.role === 'admin'){
-                isAdmin= true;
-            }
-            res.json({admin: isAdmin});
-        })
-        app.put('/users/admin',async(req,res)=>{
-            const user = req.body; 
-            const filter = {email: user.email};
-            const updateDoc = {$set: {role: 'admin'}};
-            const result = await usersCollection.updateOne(filter,updateDoc);
-            res.json(result);
 
+        app.put('/users',async (req,res)=>{
+            const user = req.body;
+            const filter = {email: user.email};
+            const options = {upsert: true};
+            const updateDoc = {$set: user};
+            const result = usersCollection.updateOne(filter,updateDoc,options);
+                res.json(result);
         })
+        // app.get('/users/:email', async(req,res) =>{
+        //     const email = req.params.email;
+        //     const query = {email: email};
+        //     const user = await usersCollection.findOne(query);
+        //     let isAdmin = false;
+        //     if(user?.role === 'admin'){
+        //         isAdmin= true;
+        //     }
+        //     res.json({admin: isAdmin});
+        // })
+        // app.put('/users/admin',async(req,res)=>{
+        //     const user = req.body; 
+        //     const filter = {email: user.email};
+        //     const updateDoc = {$set: {role: 'admin'}};
+        //     const result = await usersCollection.updateOne(filter,updateDoc);
+        //     res.json(result);
+
+        // })
     }
     finally {
 
